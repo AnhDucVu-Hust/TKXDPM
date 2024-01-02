0c8b0be6142a9c7b466235c7a7bd6aa3ebf9a550
AnhDucVu <88773938+AnhDucVu-Hust@users.noreply.github.com>
2024-01-02 14:44:46 +0700

    up again

---
+++ BTL_TKXDPM/src/main/java/com/btl_tkxdpm/export/BangChamCongNVVP.java
@@ -0,0 +1,11 @@
+package com.btl_tkxdpm.export;
+
+import com.btl_tkxdpm.AttendanceDB.IAttendanceDB;
+import com.btl_tkxdpm.entity.NhanVienVanPhongThongKe;
+import javafx.collections.ObservableList;
+
+public class BangChamCongNVVP {
+    public static ObservableList<NhanVienVanPhongThongKe> getBangChamCong(IAttendanceDB attendanceDB){
+
+    }
+}
